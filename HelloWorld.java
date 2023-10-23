0c00d2cf1f839d8034dd565a24ced687e4fd0392
Ömer ŞENTÜRK <116058861+senturk20@users.noreply.github.com>
2023-10-23 14:45:10 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,25 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide a language choice as a command-line argument (e.g., 1 for English, 2 for Spanish).");
+            return;
+        }
 
-        System.out.println("Hello world");
+        int choice = Integer.parseInt(args[0]);
+
+        String greeting = "";
+
+        if (choice == 1) {
+            greeting = "Hello, World!";
+        } else if (choice == 2) {
+            greeting = "¡Hola, Mundo!";
+        } else if (choice == 3) {
+            greeting = "Bonjour, le Monde!";
+        } else {
+            System.out.println("Invalid choice. Defaulting to English.");
+            greeting = "Hello, World!";
+        }
+
+        System.out.println(greeting);
     }
 }
